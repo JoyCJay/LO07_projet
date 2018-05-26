5870953c829f36e4789991c4109dbe9ec99358dd
xiaotongSONG <xiaotong.song@utt.fr>
2018-05-26 15:15:09 +0200

    5.26

--- ajouter_enfant.php
+++ ajouter_enfant.php
@@ -1,69 +1,42 @@
 <!DOCTYPE html>
 <html>
 <head>
-    
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Gerer mes enfants</title>
+    <title>Nounous candidat</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/form.css">
+    <link rel="stylesheet" href="css/main.css">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="js/bootstrap.js"></script>
     <script src="js/self.js"></script>
     <!--
-        http://localhost/LO07_projet/enfants.html
+        http://localhost/LO07_projet/main.php
     -->
 </head>
-    <body>
-    <div class="container" id="A">
-    <!-- Nav pills -->
-    <ul class="nav nav-pills" role="tablist" id="navigation">
-        <li class="nav-item">
-          <a class="nav-link active" data-toggle="pill" href="#Introduction">Introduction</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="pill" href="#menu1">Menu 1</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="pill" href="#menu2">Menu 2</a>
-        </li>
-    </ul> 
-    <!-- Tab panes -->
-    <div class="tab-content">
-        <div id="Introduction" class="container tab-pane active"><br>
-            <p>
-            Nounous & Moi a été fondée en 2018 et a été largement saluée par les clients depuis sa création. Permettre aux clients de sélectionner leurs nounous sans quitter leur domicile et effectuer les opérations
-            </p>
-        </div>
-        <div id="menu1" class="container tab-pane fade"><br>
-            <h3>Menu 1</h3>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
-        <div id="menu2" class="container tab-pane fade"><br>
-            <h3>Menu 2</h3>
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-        </div>
-    </div>
+<body>
+<div class="container" id="A">
+   
     <a  href="http://localhost/LO07_projet/index.html"><img class='logo' src="img/logo.png"></a>
-    </div>
+</div>
 
 <div class="container" id="B">
     <img id='profil' src="img/profil.jpg">
     <div id="profil_info">
-        Song Xiaotong Parents
+        <?php session_start(); echo "Bonjour, <br />".$_SESSION['login'];?>
     </div>
     <div id="accordion" class="panel_B">
           <div class="card" id="panel_compte">
             <div class="card-header">
               <a class="card-link B" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-                Gerer mes enfants
+                Gerer mes compt
               </a>
             </div>
             <div id="collapseOne" class="collapse">
               <div class="card-block">
-                <a class="B" href="https://www.baidu.com">Mes enfants</a></br>
-                <a class="B" href="./ajouter_enfant.html">Ajouter_enfants</a>
+                <a class="B" href="#" onclick="enfants()">Mes informations</a></br>
+                <a class="B" href="./enfant.html">Ajouter un(e) enfants</a>
               </div>
             </div>
           </div>
@@ -71,12 +44,12 @@
           <div class="card" id="panel_histoire">
             <div class="card-header">
               <a class="collapsed card-link B" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-              Histoire
+              Gerer mes réservations
             </a>
             </div>
             <div id="collapseTwo" class="collapse">
               <div class="card-block">
-              collapseTwo
+              <a class="B" href="#" onclick="contrats()">Mes réservations</a>
               </div>
             </div>
           </div>
@@ -95,81 +68,43 @@
           </div>
       </div>
 </div>
+
  <div class="container-fluid" id="C">
-	<h2 class="text-center">
-        <b>Mes enfants</b>
-	</h2>
+	<h3 >
+       Ajouter un(e) enfant
+	</h3>
        
             
 <?php
   global $_POST;
+ 
+  $login=$_SESSION['login'];
  function insert_enfants_DBR(){
      global $_POST; 
-      global $push; 
+     global $login;
       $DB_conn = mysqli_connect ('localhost','solange','abc1234567','nounous');
-        $sql_sentence = "INSERT INTO `enfant` (`id_enfant`, `prenom`, `parents`, `date_de_naissance`, `restrictions_alimentaires`) VALUES("
-            ."'".$push['id_enfant']."',"
-            ."'".$_POST['prenom']."',"
-            ."'".$_POST['parents']."',"
-            ."'".$_POST['naissance']."',"
-            ."'".$_POST['ra']."'"
-            .");";
-        echo $sql_sentence;
-        $DB_result = mysqli_query($DB_conn,$sql_sentence);          
+        $sql= "INSERT INTO `enfant` (`id_enfant`, `prenom`, `parents`, `date_de_naissance`, `restrictions_alimentaires`) VALUES( NULL ,
+            '".$_POST['prenom'] ."',
+            '".$login ."',
+             '".$_POST['naissance'] ."',
+             '".$_POST['ra'] ."'
+           );";
+        
+        $result = mysqli_query($DB_conn,$sql);          
         mysqli_close($DB_conn);
     }
+    insert_enfants_DBR();
+    echo "<ul>Informations du enfant:";
 
-    function table(){
-        global $_POST; 
-        $push = array(
-                "id_enfant"=>"",
-                "prenom" => $_POST['prenom'],
-                "parents" => $_POST['parents'],
-                "naissance" => $_POST['naissance'],
-                "ra" => $_POST['ra']
-            );
-        insert_enfants_DBR();
-    }
+    echo "<li>Prénom: ".$_POST['prenom']; 
+    echo "</li>";
+    echo "<li>Date de naissance: ".$_POST['naissance'];
+    echo "</li>";
+    echo "<li>Restrictions alimentaires: ".$_POST['ra'];
+    echo "</li>";
+    echo "</ul><br />";
+    echo "<input type='button' name='rentrer' onclick='javascript:history.back(-1);' value='Rentrer' >";
 
-  
-  function list_enfant(){
-      global $_POST;
-      global $push;
-      $DB_conn = mysqli_connect ('localhost','solange','abc1234567','nounous');
-      $sql_sentence= "SELECT * FROM `enfant` WHERE `parents`= '".$_POST['parents']."'";
-      $DB_result = mysqli_query($DB_conn,$sql_sentence);  
-             echo "<table class='table'>";
-              echo "<thead><tr>";
-              echo "<th>ID_enfants</th>"; 
-              echo "<th>Prénom des enfants</th>"; 
-              echo "<th>Date de naissance</th>"; 
-              echo "<th>Restrictions alimentaires</th>"; 
-              echo "</tr></thead>"; 
-              echo "<tbody>";   
-             while($temp = mysqli_fetch_array ($DB_result,MYSQLI_ASSOC)){
-                  echo "<tr>";
-                  echo "<td>";
-                  echo $temp['id_enfant'];
-                  echo "</td>";
-                  echo "<td>";
-                  echo $temp['prenom'];
-                  echo "</td>";
-                  echo "<td>";
-                  echo $temp['date_de_naissance'];
-                  echo "</td>";
-                  echo "<td>";
-                  echo $temp['restrictions_alimentaires'];
-                  echo "</td>";
-
-            }
-            echo "</tbody>"; 
-            echo "</table>";
-        mysqli_close($DB_conn);
-  }
-
-
-  table();
-  list_enfant();
   ?>
 			
 		</div>
